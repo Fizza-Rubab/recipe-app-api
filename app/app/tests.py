0a357e94a77cf69cd532c6379b78ba1008d5eb0f
Fizza-Rubab <fr06161@st.habib.edu.pk>
2023-01-21 12:21:45 +0500

    added healthcheck for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,9 +4,9 @@ from app import calc
 class CalcTests(SimpleTestCase):
     '''Test the calc module'''
     def test_calc_add(self):
-        res = calc.add(7,4)
-        self.assertEqual(res,11)
+        res = calc.add(7, 4)
+        self.assertEqual(res, 11)
 
     def test_calc_subtract(self):
-        res = calc.subtract(4,3)
+        res = calc.subtract(4, 3)
         self.assertEqual(res, 1)
